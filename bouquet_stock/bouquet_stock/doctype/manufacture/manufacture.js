0ef8676131acf15a378b1bf4c20e1e00499375e5
krisnajufer <krisnajufer07@gmail.com>
2026-01-03 15:17:38 +0700

    UPDATE: calculate material bouquet

--- bouquet_stock/bouquet_stock/doctype/manufacture/manufacture.js
+++ bouquet_stock/bouquet_stock/doctype/manufacture/manufacture.js
@@ -1,8 +1,47 @@
 // Copyright (c) 2025, Jufer and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Manufacture", {
-// 	refresh(frm) {
+frappe.ui.form.on("Manufacture", {
+	refresh(frm) {
 
-// 	},
-// });
+	},
+});
+
+frappe.ui.form.on("Manufacture Bouquet Item", {
+	refresh(frm) {
+
+	},
+
+    bouquet(frm, cdt, cdn){
+        calculateMaterial(frm, cdt, cdn)
+    },
+    qty(frm, cdt, cdn){
+        calculateMaterial(frm, cdt, cdn)
+    }
+});
+
+async function calculateMaterial(frm, cdt, cdn) {
+    const curRow = locals[cdt][cdn]
+    if (curRow.qty > 0) {
+        const result = await frappe.call({
+            method: 'bouquet_stock.bouquet_stock.doctype.manufacture.manufacture.calculate_material_bouquet',
+            args: {
+                bouquet_name: curRow.bouquet,
+                bouquet_qty: curRow.qty
+            }
+        })
+        let status = "Bisa"
+        frm.doc.materials = []
+        for (const row of result.message) {
+            if (row.status == "Tidak Cukup") {
+                status = "Tidak Bisa"
+            }
+            frm.add_child("materials", row)
+        }
+
+        curRow.status = status;
+        frm.refresh_field("bouquets");
+        frm.refresh_field("materials");
+    }
+    
+}
--- bouquet_stock/bouquet_stock/doctype/manufacture/manufacture.py
+++ bouquet_stock/bouquet_stock/doctype/manufacture/manufacture.py
@@ -1,9 +1,28 @@
 # Copyright (c) 2025, Jufer and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Manufacture(Document):
 	pass
+
+@frappe.whitelist()
+def calculate_material_bouquet(bouquet_name, bouquet_qty):
+	bouquet = frappe.get_doc("Bouquet", bouquet_name)
+
+	material_needs = []
+	for row in bouquet.bouquet_material:
+		actual_qty = frappe.db.get_value("Material Stock", {"material": row.material}, "actual_qty")
+		value = {
+			"material": row.material,
+			"qty": float(row.qty) * float(bouquet_qty),
+			"current_qty": actual_qty,
+			"status": "Cukup"
+		}
+		if value["current_qty"] < value["qty"]:
+			value["status"] = "Tidak Cukup"
+		material_needs.append(value)
+
+	return material_needs
